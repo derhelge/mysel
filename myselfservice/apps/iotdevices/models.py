10817a451c551d2ea36679881e629b2bdaa47513
Helge Wiethoff <helge.wiethoff@thga.de>
2025-08-25 14:06:01 +0200

    Fix: MAC-Adressen werden validiert

--- myselfservice/apps/iotdevices/models.py
+++ myselfservice/apps/iotdevices/models.py
@@ -1,15 +1,26 @@
 from django.conf import settings 
 from django.db import models
 from apps.core.models import BaseAccountModel
+import re
+from django.core.exceptions import ValidationError
 
 class IotDeviceAccount(BaseAccountModel):
     device_name = models.CharField(max_length=510, blank=True, verbose_name='Gerätename')
     username = None
     mac_address = models.CharField(
-        max_length=17, 
+        max_length=17,
         unique=False,
-        verbose_name='MAC-Adresse'
-        )
+        verbose_name='MAC-Adresse',
+    )
+
+    def clean(self):
+        super().clean()
+        if self.mac_address:
+            clean = re.sub(r'[^0-9A-F]', '', self.mac_address.upper())
+            if len(clean) != 12:
+                raise ValidationError({'mac_address': 'MAC-Adresse muss 12 Hex-Zeichen enthalten.'})
+            self.mac_address = ':'.join([clean[i:i+2] for i in range(0, 12, 2)])
+
     class Meta:
         verbose_name = 'IoT Device Account'
         verbose_name_plural = 'IoT Device Accounts'
